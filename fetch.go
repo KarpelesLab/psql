de20802649f5853df46524621b29127b878dd895
Mark Karpeles <magicaltux@gmail.com>
2022-10-29 23:22:40 +0900

    add Get to load+spawn

--- fetch.go
+++ fetch.go
@@ -12,8 +12,44 @@ func FetchOne[T any](ctx context.Context, target *T, where map[string]any) error
 	return Table[T]().FetchOne(ctx, target, where)
 }
 
+// Get will instanciate a new object of type T and return a pointer to it after loading from database
+func Get[T any](ctx context.Context, where map[string]any) (*T, error) {
+	return Table[T]().Get(ctx, where)
+}
+
 func Fetch[T any](ctx context.Context, where map[string]any) ([]*T, error) {
 	return Table[T]().Fetch(ctx, where)
+}
+
+func (t *TableMeta[T]) Get(ctx context.Context, where map[string]interface{}) (*T, error) {
+	// simplified get
+	req := "SELECT " + t.fldStr + " FROM " + QuoteName(t.table)
+	var params []interface{}
+	if where != nil {
+		var whQ []string
+		for k, v := range where {
+			whQ = append(whQ, QuoteName(k)+"=?")
+			params = append(params, v)
+		}
+		if len(whQ) > 0 {
+			req += " WHERE " + strings.Join(whQ, " AND ")
+		}
+	}
+	req += " LIMIT 1"
+
+	// run query
+	rows, err := db.QueryContext(ctx, req, params...)
+	if err != nil {
+		log.Printf("[sql] error: %s", err)
+		return nil, &Error{Query: req, Err: err}
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		// no result
+		return nil, os.ErrNotExist
+	}
+	return t.spawn(rows)
 }
 
 func (t *TableMeta[T]) FetchOne(ctx context.Context, target *T, where map[string]interface{}) error {
