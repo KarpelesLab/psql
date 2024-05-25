3d192f8fd21d622531a23a7491893f8c34c9466a
Mark Karpeles <magicaltux@gmail.com>
2024-05-25 16:33:32 +0900

    add pg connect command

--- go.mod
+++ go.mod
@@ -8,10 +8,16 @@ require (
 	github.com/KarpelesLab/pjson v0.1.9
 	github.com/KarpelesLab/typutil v0.2.8
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/jackc/pgx/v5 v5.5.5
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package psql
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log/slog"
+	"strings"
 	"time"
 
 	"github.com/go-sql-driver/mysql"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
 )
 
-var db *sql.DB
+var (
+	db   *sql.DB
+	pgdb *pgxpool.Pool
+)
 
 // Init starts the database pool and allows all other methods in psql to work
 func Init(dsn string) error {
+	if strings.HasPrefix(dsn, "postgresql://") {
+		cfg, err := pgxpool.ParseConfig(dsn)
+		if err != nil {
+			return err
+		}
+		return InitPG(cfg)
+	}
 	cfg, err := mysql.ParseDSN(dsn)
 	if err != nil {
 		return err
@@ -50,5 +64,15 @@ func InitCfg(cfg *mysql.Config) error {
 		slog.Debug(fmt.Sprintf("[mysql] %s = %s", k, v), "event", "psql:init:dbvar", "psql.dbvar", k)
 	}
 
+	return nil
+}
+
+func InitPG(cfg *pgxpool.Config) error {
+	var err error
+	pgdb, err = pgxpool.NewWithConfig(context.Background(), cfg)
+	if err != nil {
+		return err
+	}
+	db = stdlib.OpenDBFromPool(pgdb)
 	return nil
 }
